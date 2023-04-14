96306e3c594fb70beb3b0a8a1c56e9d58b639fe1
admin <bindocrishjo14@gmail.com>
2023-04-14 11:00:52 +0530

    test git

---
+++ lib/language_translation.dart
@@ -0,0 +1,49 @@
+import 'package:get/get.dart';
+class LocaleString extends Translations{
+  @override
+
+  Map<String, Map<String, String>> get keys => {
+  //ENGLISH LANGUAGE
+  'en_US':{
+  'hello':'Hello Transformative Power to Enterprises!',
+  'message':'Myself i m Bindo form Gudalur. And i m completed MCA I have one year experience in flutter development',
+  'title':"Translator Project",
+  'sub':'Subscribe Now',
+  'changelang':'Change Language'
+  },
+  //HINDI LANGUAGE
+  'hi_IN':{
+  'hello': 'नमस्ते दुनिया',
+  'message':'प्रोटो कोडर प्वाइंट में आपका स्वागत है',
+  'title':'स्पंदन भाषा - स्थानीयकरण',
+  'subscribe':'सब्सक्राइब',
+  'changelang':'भाषा बदलो'
+  },
+  //KANNADA LANGUAGE
+  'kn_IN':{
+
+    'hello': 'ಹಲೋ ವರ್ಲ್ಡ್',
+    'message':'ಪ್ರೋಟೋ ಕೋಡರ್ ಪಾಯಿಂಟ್‌ಗೆ ಸುಸ್ವಾಗತ',
+    'title':'ಬೀಸು ಭಾಷೆ - ಸ್ಥಳೀಕರಣ',
+    'subscribe':'ವಂತಿಗೆ ಕೊಡು',
+    'changelang':'ಭಾಷೆ ಬದಲಿಸಿ'
+  },
+    'ml_IN':{
+
+      'hello': 'ഹലോ ട്രാൻസ്ഫോർമേറ്റീവ് പവർ എന്റർപ്രൈസസ്',
+      'message':'idea ila',
+      'title':'ಬೀಸು ಭಾಷೆ - ಸ್ಥಳೀಕರಣ',
+      'subscribe':'ವಂತಿಗೆ ಕೊಡು',
+      'changelang':'ಭಾಷೆ ಬದಲಿಸಿ'
+
+    },
+    'tn_IN':{
+
+      'hello': 'ஹலோ ட்ரான்ஸபோர்மடிவ் பவர் என்டர்ப்ரிஸ்ஸ்',
+      'message':'plane pannala',
+      'title':'ಬೀಸು ಭಾಷೆ - ಸ್ಥಳೀಕರಣ',
+      'subscribe':'ವಂತಿಗೆ ಕೊಡು',
+      'changelang':'ಭಾಷೆ ಬದಲಿಸಿ'
+    }
+  };
+}
